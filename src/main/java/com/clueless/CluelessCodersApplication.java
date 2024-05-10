c7a96cd17be7082ae4518fbca15eda7919b1e772
s-gaikwad <shashank.gaikwad@apptware.com>
2024-05-11 00:37:49 +0530

    Initial Commit.

---
+++ src/main/java/com/clueless/CluelessCodersApplication.java
@@ -0,0 +1,13 @@
+package com.clueless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluelessCodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluelessCodersApplication.class, args);
+	}
+
+}
